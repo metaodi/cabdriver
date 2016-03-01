8076ee511a6c476eef682509be1598c3b096b559
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-03-01 20:14:02 +0100

    Fix date parsing

---
+++ README.md
@@ -0,0 +1,43 @@
+cabdriver
+=========
+
+cabdriver is a small helper application that helps you to fill in your hours in taxi.
+It currently support Google Calendar to get entries in a taxi-friendly format.
+
+## Usage
+
+```bash
+$ cabdriver -c 10 -d last-month
+
+02/02/2016 # Tuesday
+xxx    09:00-10:00   opendata.swiss Go-Live
+xxx    09:30-09:45   Jazz Daily Stand-Up
+xxx    10:05-10:30   Weiteres Vorgehen Vowi
+xxx    10:30-12:00   Analyse Altium
+xxx    13:30-14:00   IPA-Besprechung
+xxx    16:00-19:00   Byebye Apero Lukas
+
+03/02/2016 # Wednesday
+xxx    09:30-09:45   Jazz Daily Stand-Up
+xxx    10:00-10:30   Support Backlog
+xxx    10:45-11:30   HWZ Preplanning
+xxx    14:00-15:00   HWZ DoD
+```
+
+## Options
+
+* `-d` supports date strings or ranges:
+
+** 31.12.2016
+** 01.12.2016-31.12.2016
+** yesterday
+** last-week
+** past-week (7 days)
+** last-month (month before the current)
+** past-month (30 days)
+** last-year (year before the current)
+** past-year (365 days)
+** today (up to current time)
+** this-week (up to current time)
+** this-month (up to current time)
+** this-year (up to current time)
--- cabdriver.js
+++ cabdriver.js
@@ -4,19 +4,24 @@ var Program = require('commander');
 var Chrono = require('chrono-node');
 var SemanticDate = require('semantic-date');
 var Moment = require('moment-timezone');
+var _ = require('underscore');
 
 var auth = require('./lib/auth');
 var calendar = require('./lib/calendar');
 
 function dashed(val) {
-    return val.split('-').join(' ');
+    var splitted = val.split('-');
+    return _.map(splitted, function(elem) {
+        return elem.trim();
+    });
 }
 
 Program
   .version('0.0.1')
   .option('-c, --count [number of events]', 'Amount of events to show [10]', 10)
-  .option('-d, --date <date>', 'Date for query [today]', dashed, 'today')
+  .option('-d, --date <date>', 'Date for query [today]', 'today')
   .option('-c, --calendar [cal_id]', 'Determine which calendar you want to use [primary]', 'primary')
+  .option('-v, --verbose', 'More verbose output [false]', false)
   .parse(process.argv);
 
 
@@ -28,20 +33,30 @@ if (SemanticDate.validate(Program.date)) {
     startDate = parsed.start;
     endDate = parsed.end;
 } else {
-    var dates = Program.date.split(' ');
+    var dates = dashed(Program.date);
     var startStr = dates[0];
-    var endStr = dates[1]
+    var endStr = '';
+    if (dates.length > 1) {
+        endStr = dates[1].trim();
+    }
 
     if (startStr && Moment(startStr, 'DD.MM.YYYY').isValid()) {
         startDate = Moment(startStr, 'DD.MM.YYYY');
     }
     if (endStr && Moment(endStr, 'DD.MM.YYYY').isValid()) {
-        endDate = Moment(endStr, 'DD.MM.YYYY').endOf('day');
+        endDate = Moment(endStr, 'DD.MM.YYYY');
     }
 }
 
 startDate = Moment(startDate).toISOString();
-endDate = endDate ? Moment(endDate).toISOString() : '';
+endDate = endDate ? Moment(endDate).endOf('day').toISOString() : '';
+
+if (Program.verbose) {
+    console.log('Start date: %s', startDate);
+    console.log('End date: %s', endDate);
+    console.log('Calendar: %s', Program.calendar);
+    console.log('Count: %s', Program.count);
+}
 
 if (Program.count) {
     auth.getAuth(function(auth) {
