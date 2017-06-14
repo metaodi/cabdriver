58c0d7249c04371746d3fbc4f0911262a0753b49
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-06-14 13:31:29 +0200

    Add pie test for slack

--- test/test.slack.js
+++ test/test.slack.js
@@ -51,6 +51,45 @@ describe('Slack', function() {
             };
             Sinon.assert.calledWith(callback, null, [msg]);
         });
+        it('generates pie based on msgs', function(done) {
+            var searchStub = sandbox.stub(SlackApi.search, 'messages').yields(
+                null,
+                {
+                    'messages': {
+                        'matches': [{
+                            'ts': 1497045600000,
+                            'type': 'message',
+                            'channel': {'id': 'C03PL5HE8'}
+                        }]
+                    }
+                }
+            );
+            var channelStub = sandbox.stub(SlackApi.channels, 'info').yields(
+                null,
+                {
+                    'channel': {'name': 'testchannel'}
+                }
+            );
+            
+            var options = {
+                'startDate': '2017-04-09',
+                'endDate': '2017-04-11',
+                'slack': true,
+                'pie': true
+            };
+            slack.dailyStats(function(err, result) {
+                try {
+                    var entry = result[0];
+                    expect(err).to.not.exist;
+                    expect(entry.timestamp).to.equal('1497045538800');
+                    expect(entry.type).to.equal('slack');
+                    expect(entry.raw).to.exist;
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, '1234', options);
+        });
     });
 });
 
