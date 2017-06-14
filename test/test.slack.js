6788ddc21803580b5a93e569ce7d127c4eee7869
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-06-14 22:38:38 +0200

    Add slack test for private messages

--- test/test.slack.js
+++ test/test.slack.js
@@ -13,7 +13,7 @@ describe('Slack', function() {
     });
 
     describe('dailyStats', function() {
-        it('returns the correct msgs', function() {
+        it('returns the correct entries for public msgs', function() {
             var callback = sandbox.spy();
             var searchStub = sandbox.stub(SlackApi.search, 'messages').yields(
                 null,
@@ -46,6 +46,51 @@ describe('Slack', function() {
                 'text': '1 messages in #testchannel',
                 'graph': { 'label': "testchannel - 1", 'value': 1 },
                 'timestamp': "1497045538800",
+                'comment': false,
+                'type': 'slack'
+            };
+            Sinon.assert.calledWith(callback, null, [msg]);
+        });
+        it('returns the correct entries for private msgs', function() {
+            var callback = sandbox.spy();
+            var searchStub = sandbox.stub(SlackApi.search, 'messages').yields(
+                null,
+                {
+                    'messages': {
+                        'matches': [
+                            {
+                                'ts': 1497045900000,
+                                'type': 'im',
+                                'channel': {'id': 'I03PL5HE8'}
+                            },
+                            {
+                                'ts': 1497045910000,
+                                'type': 'im',
+                                'channel': {'id': 'I03PL5HE8'}
+                            }
+                        ]
+                    }
+                }
+            );
+            var channelStub = sandbox.stub(SlackApi.users, 'info').yields(
+                null,
+                {
+                    'user': {'name': 'testuser'}
+                }
+            );
+            
+            var options = {
+                'startDate': '2017-04-09',
+                'endDate': '2017-04-11',
+                'slack': true
+            };
+            slack.dailyStats(callback, '1234', options);
+            var msg = {
+                'project': 'xxx',
+                'time': '',
+                'text': '2 messages with testuser',
+                'graph': { 'label': "testuser - 2", 'value': 2 },
+                'timestamp': "1497045884400",
                 'comment': false,
                 'type': 'slack'
             };
