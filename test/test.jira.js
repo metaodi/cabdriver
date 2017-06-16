d5c17127f00b7259d7421dffe85f7d0ef95b9bf7
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-06-16 23:18:16 +0200

    Add worklog and comment test for jira

--- test/test.jira.js
+++ test/test.jira.js
@@ -66,5 +66,131 @@ describe('Jira', function() {
                 }
             }, auth, options);
         });
+        it('generates entries based on Jira comments', function(done) {
+            this.timeout(5000);
+            var currentUserStub = sandbox.stub(JiraApi.prototype, 'getCurrentUser')
+                .resolves({'key': 'testuser'});
+            var searchJiraStub = sandbox.stub(JiraApi.prototype, 'searchJira')
+                .resolves({'issues': [{'key': 'TEST-124'}]});
+            var findIssueStub = sandbox.stub(JiraApi.prototype, 'findIssue')
+                .resolves({
+                    'key': 'TEST-123',
+                    'changelog': {
+                        'histories': [{
+                            'author': {'key': 'anotheruser'},
+                            'created': '2017-03-22'
+                        }]
+                    },
+                    'fields': {
+                        'summary': 'Test issue summary',
+                        'comment': {'comments': [
+                            {
+                                'created': '2017-03-29',
+                                'author': {'key': 'anotheruser'}
+                            },
+                            {
+                                'created': '2017-03-29',
+                                'author': {'key': 'testuser'}
+                            },
+                            {
+                                'created': '2017-03-31',
+                                'author': {'key': 'testuser'}
+                            }
+                        ]},
+                        'worklog': {'worklogs': []}
+                    }
+                });
+
+
+            var options = {
+                'startDate': '2017-03-28',
+                'endDate': '2017-03-30',
+                'jira': true
+            };
+            var auth = {
+                'consumer_key': '123',
+                'consumer_secret': 'secret',
+                'access_token': '1234',
+                'access_token_secret': 'secret'
+            };
+            jira.getActivities(function(err, result) {
+                try {
+                    expect(err).to.not.exist;
+                    expect(result).to.deep.equal([
+                        {
+                            project: 'test',
+                            time: '1',
+                            text: 'TEST-123: Test issue summary',
+                            timestamp: '1490738400',
+                            type: 'jira',
+                            comment: false
+                        }
+                    ]);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, auth, options);
+        });
+        it('generates entries based on Jira worklog', function(done) {
+            this.timeout(5000);
+            var currentUserStub = sandbox.stub(JiraApi.prototype, 'getCurrentUser')
+                .resolves({'key': 'testuser'});
+            var searchJiraStub = sandbox.stub(JiraApi.prototype, 'searchJira')
+                .resolves({'issues': [{'key': 'TEST-124'}]});
+            var findIssueStub = sandbox.stub(JiraApi.prototype, 'findIssue')
+                .resolves({
+                    'key': 'TEST-123',
+                    'changelog': {
+                        'histories': [{
+                            'author': {'key': 'anotheruser'},
+                            'created': '2017-03-22'
+                        }]
+                    },
+                    'fields': {
+                        'summary': 'Test issue summary',
+                        'comment': {'comments': []},
+                        'worklog': {'worklogs': [
+                            {
+                                'started': '2017-03-29',
+                                'author': {'key': 'testuser'},
+                                'timeSpentSeconds': 7200,
+                                'comment': 'Problem analysis'
+                            }
+                        ]}
+                    }
+                });
+
+
+            var options = {
+                'startDate': '2017-03-28',
+                'endDate': '2017-03-30',
+                'jira': true
+            };
+            var auth = {
+                'consumer_key': '123',
+                'consumer_secret': 'secret',
+                'access_token': '1234',
+                'access_token_secret': 'secret'
+            };
+            jira.getActivities(function(err, result) {
+                try {
+                    expect(err).to.not.exist;
+                    expect(result).to.deep.equal([
+                        {
+                            project: 'test',
+                            time: '2',
+                            text: 'TEST-123 worklog: Problem analysis',
+                            timestamp: '1490738400',
+                            type: 'jira',
+                            comment: false
+                        }
+                    ]);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            }, auth, options);
+        });
    });
 });
