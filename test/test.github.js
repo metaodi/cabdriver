48fa3a3cb97be18a7912493839bd8d1d66043935
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-06-27 18:39:56 +0200

    Update test to stub events

--- test/test.github.js
+++ test/test.github.js
@@ -3,7 +3,7 @@ var Sinon = require('sinon');
 var expect = require('chai').expect;
 
 var github = require('../lib/github');
-var GithubApi = require('github-api');
+var GithubApi = require('github');
 
 var sandbox = Sinon.sandbox.create();
 
@@ -13,38 +13,39 @@ describe('Github', function() {
     });
 
     describe('getContributions', function() {
-        it('returns the correct entry for a created issue', function(done) {
-            // setup stubs
-            var userStub = {
-                'getProfile': sandbox.stub().resolves(
-                    {'data': {'login': 'githubtestuser'}}
-                )
+        it('returns the correct entry for a PullRequestEvent', function(done) {
+            // setup stubs  
+            var userStub = sandbox.stub().resolves({
+                'data': {
+                    'login': 'githubtestuser'
+                }
+            });
+            var eventStub = sandbox.stub().resolves({
+                'data': [{
+                    'actor': {
+                        'login': 'githubtestuser'
+                    },
+                    'created_at': '2017-06-23',
+                    'repo': {
+                        'name': 'githubtestuser/test-repo'
+                    },
+                    'type': 'PullRequestEvent',
+                    'payload': {
+                        'action': 'created',
+                        'pull_request': {
+                            'number': '1337',
+                            'title': 'Test pull request'
+                        }
+                    }
+                }]
+            });
+            var apiStub = { 
+                'hasNextPage': sandbox.stub().returns(false),
+                'users': {'get': userStub},
+                'activity': {'getEventsForUser': eventStub}
             };
-            var searchStub = {
-                'forIssues': sandbox.stub().resolves(
-                    {
-                        'data': [
-                            {
-                                'repository_url': 'https://github.com/metaodi/cabdriver',
-                                'number': '1337',
-                                'created_at': '2017-06-23',
-                                'title': 'Test github issue',
-                                'user': {'login': 'githubtestuser'}
-                            }
-                        ]
-                    })
-            };
-            var issuesStub = {
-                'listIssueComments': sandbox.stub().resolves([]),
-                'listIssueEvents': sandbox.stub().resolves([])
-            };
-            var githubStub = {
-                'getUser': sandbox.stub().returns(userStub),
-                'search': sandbox.stub().returns(searchStub),
-                'getIssues': sandbox.stub().returns(issuesStub)
-            };
-            var apiStub = sandbox.stub(github, 'getApi').returns(githubStub)
-            
+            sandbox.stub(github, 'getApi').returns(apiStub);
+
             // call the function
             var options = {
                 'startDate': '2017-06-22',
@@ -53,17 +54,21 @@ describe('Github', function() {
             };
             github.getContributions(
                 function(err, result) {
-                    expect(err).to.not.exist;
-                    var msg = {
-                        'project': 'cabdriver',
-                        'time': '1',
-                        'text': '#1337: Test github issue',
-                        'timestamp': "1498168800",
-                        'comment': false,
-                        'type': 'github'
-                    };
-                    expect(result).to.be.deep.equal([msg]);
-                    done();
+                    try {
+                        expect(err).to.not.exist;
+                        var msg = {
+                            'project': 'test-repo',
+                            'time': '1',
+                            'text': '#1337: Test pull request, pull request created',
+                            'timestamp': "1498168800",
+                            'comment': false,
+                            'type': 'github'
+                        };
+                        expect(result).to.be.deep.equal([msg]);
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
                 },
                 '1234',
                 options
