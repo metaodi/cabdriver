6dfc416a3bf4949c27d242fde8b4d54bd3d22d28
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-04-05 22:59:49 +0200

    Add pagination to calendar source

--- test/test.calendar.js
+++ test/test.calendar.js
@@ -14,7 +14,8 @@ describe('Calendar', function() {
     });
 
     describe('generateEntries', function() {
-        it('returns the correct msgs with hour option', function() {        //setup stubs
+        it('returns the correct msgs with hour option', function() {
+            //setup stubs
             var listStub = sandbox.stub().yields(
                 null,
                 {
@@ -51,7 +52,8 @@ describe('Calendar', function() {
                     expect(results).to.be.deep.equal([msg]);
                 });
         });
-        it('returns the correct msgs without hour option', function() {        //setup stubs
+        it('returns the correct msgs without hour option', function() {
+            //setup stubs
             var callback = sandbox.spy();
             var listStub = sandbox.stub().yields(
                 null,
@@ -86,6 +88,77 @@ describe('Calendar', function() {
                         'type': 'calendar'
                     };
                     expect(results).to.be.deep.equal([msg]);
+                });
+        });
+        it('returns the correct msgs with pagination', function() {
+            //setup stubs
+            var callback = sandbox.spy();
+            var listStub = sandbox.stub();
+            listStub.onCall(0).yields(
+                null,
+                {
+                    'nextPageToken': 'aaabbbcccddd',
+                    'items': [
+                        {
+                            'start': {'dateTime': '2017-05-17T13:45:00Z'},
+                            'end': {'dateTime': '2017-05-17T14:00:00Z'},
+                            'summary': 'Test Event',
+                        }
+                    ]
+                }
+            );
+            listStub.onCall(1).yields(
+                null,
+                {
+                    'items': [
+                        {
+                            'start': {'dateTime': '2017-05-17T15:45:00Z'},
+                            'end': {'dateTime': '2017-05-17T16:00:00Z'},
+                            'summary': 'Test Event Page 2',
+                        }
+                    ]
+                }
+            );
+            var googleStub = sandbox.stub(Google, 'calendar').returns({'events': {'list': listStub}});
+            
+            var options = {
+                'count': 11,
+                'startDate': '2017-05-17',
+                'calendar': 'events',
+            };
+            var authStub = {'getAuth': sandbox.stub().resolves('1234')};
+            var calendar = new GoogleCalendar(options, authStub);
+
+            return calendar.getEntries()
+                .then(function(results) {
+                    var msgs = [
+                        {
+                            'project': 'xxx',
+                            'time': '15:45-16:00',
+                            'text': 'Test Event',
+                            'timestamp': "1494972000",
+                            'comment': false,
+                            'type': 'calendar'
+                        },
+                        {
+                            'project': 'xxx',
+                            'time': '17:45-18:00',
+                            'text': 'Test Event Page 2',
+                            'timestamp': "1494972000",
+                            'comment': false,
+                            'type': 'calendar'
+                        }
+                    ];
+                    Sinon.assert.calledWith(listStub, {
+                        "auth": "1234",
+                        "calendarId": "events",
+                        "maxResults": 11,
+                        "orderBy": "startTime",
+                        "pageToken": "aaabbbcccddd",
+                        "singleEvents": true,
+                        "timeMin": "2017-05-16T22:00:00.000Z"
+					}); 
+                    expect(results).to.be.deep.equal(msgs);
                 });
         });
     });
